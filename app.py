d8220521433a3e872e905c921ac1d75512698011
Federico Gerardi <38018471+iGieri@users.noreply.github.com>
2024-03-27 20:08:10 +0100

    status, players, map api

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def join_game():
         return jsonify({'message': 'joined game, waiting for more players'})        
     if len(g.players) == 3:
         g.status = 'playing'
+        g.assigning_map_randomly()
         return jsonify({'message': 'game full, starting game'})
 
     return jsonify({'game_id': g.game_id})
@@ -59,6 +60,31 @@ def game_status(game_id):
         return jsonify({'error': 'Game not found'})
 
     return jsonify({'status': g.status})
+
+@app.route('/api/game/<game_id>/players', methods=['GET'])
+def game_players(game_id):
+    g = [g for g in games if g.game_id == game_id][0]
+
+    if g is None:
+        return jsonify({'error': 'Game not found'})
+    
+    names = [p.name for p in g.players]
+
+    return jsonify({'players': names})
+
+@app.route('/api/game/<game_id>/map', methods=['GET'])
+def game_map(game_id):
+    g = [g for g in games if g.game_id == game_id][0]
+
+    if g is None:
+        return jsonify({'error': 'Game not found'})
+    
+    new_map = g.map.copy()
+    for row in new_map:
+        row["owner"] = row["owner"].name if row["owner"] is not None else None
+
+
+    return jsonify({'map': g.map})
 
 
 if __name__ == '__main__':
