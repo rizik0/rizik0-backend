56749c3c244061726e7abca2c1ee054789aae295
Cristian Di Iorio <112891194+cristiandiiorio@users.noreply.github.com>
2024-04-16 11:16:28 +0200

    added standard database creation
    changed gitignore, changed database.sql creation

--- database.sql
+++ database.sql
@@ -1,1 +1,5 @@
 create table players(username varchar(255) primary key,email varchar(255) not null,password_hash varchar(255) not null);
+
+insert into players(username,email,password_hash) values('admin1','email1@email.com','1234');
+insert into players(username,email,password_hash) values('admin2','email2@email.com','1234');
+insert into players(username,email,password_hash) values('admin3','email3@email.com','1234');
